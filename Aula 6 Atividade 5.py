5413f32fa857257a07c4d4f45415d49c9bf623cb
rafaelatrigo <rafaelatrigo@gmail.com>
2023-09-10 16:57:52 -0300

    Atualização aula 6

---
+++ Aula 6 Atividade 5.py
@@ -0,0 +1,9 @@
+# Use um loop "while" em Python para imprimir a mensagem enquanto o valor de x não for igual a 5. 
+
+x = 0
+
+while x < 5:
+    print("O valor de X está abaixo de 5")
+    x +=1
+
+print("Fim do While")
---
+++ Aula 6 Atividade 6.py
@@ -0,0 +1,6 @@
+count = 1
+while count < 5:
+    if count != 5:
+        print("While ainda é diferente de 5")
+    count += 1 
+print ("While é igual a 5")

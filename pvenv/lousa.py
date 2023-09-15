0786df50b07d342e206cfa9dbc1b4c5977224fc1
rafaelatrigo <rafaelatrigo@gmail.com>
2023-09-15 17:48:38 -0300

    Desafio final M4

--- pvenv/lousa.py
+++ pvenv/lousa.py
@@ -1,6 +1,33 @@
-import requests
+"""import requests
 
 response = requests.get('https://viacep.com.br/ws/01001000/json/')
 data = response.json()
 
 print(data['bairro'])
+
+import pandas as pd
+
+data = {
+    'Nome': ['João','Marta', 'Ary', 'Matheus', 'Michelle'],
+    'Idade': [51, 37, 23, 24, 33],
+}
+df = pd.DataFrame(data)
+print(df[df['Idade']>30])
+
+import pandas as pd
+
+df = pd.read_csv("dados_ficticios.csv")
+for i, linha in df.iterrows():
+    print(linha['nome'], linha['renda'])"""
+
+import pandas as pd
+from faker import Faker
+faker = Faker('pt-br')
+data = {
+    'Nome': [faker.name(), faker.name()],
+    'Cidade': [faker.address(), faker.name()],
+}
+
+df = pd.DataFrame(data)
+print(df)
+df.to_csv("novaplaniha.csv")
